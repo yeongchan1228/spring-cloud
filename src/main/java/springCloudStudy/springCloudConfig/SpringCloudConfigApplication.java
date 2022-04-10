47bbf7ef7175d8029c0f40e3bed74bc5750a685c
최영찬 <gks54410@gmail.com>
2022-04-11 00:15:35 +0900

    Initial Commit

---
+++ src/main/java/springCloudStudy/springCloudConfig/SpringCloudConfigApplication.java
@@ -0,0 +1,14 @@
+package springCloudStudy.springCloudConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+public class SpringCloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springCloudStudy/springCloudConfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package springCloudStudy.springCloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
