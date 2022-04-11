de15549871a1fe64fc638cfa5728bb03309716c9
최영찬 <gks54410@gmail.com>
2022-04-11 11:24:48 +0900

    Feat: 변경 부분 확인 기능 추가

--- src/main/java/springCloudStudy/springCloudClient/controller/ConfigClientController.java
+++ src/main/java/springCloudStudy/springCloudClient/controller/ConfigClientController.java
@@ -1,13 +1,15 @@
 package springCloudStudy.springCloudClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope // Actuator 변경 감지 범위
 public class ConfigClientController {
 
-//    @Value("${test.str}")
+    @Value("${test.str}")
     private String configStr;
 
     @GetMapping("/test")
