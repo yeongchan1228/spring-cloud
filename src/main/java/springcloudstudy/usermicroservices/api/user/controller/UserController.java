e92ee520117a33ea6960d93179b7e31aa8d3d129
최영찬 <gks54410@gmail.com>
2022-04-16 19:53:43 +0900

    Feat: 회원 가입 기능 추가

--- src/main/java/springcloudstudy/usermicroservices/api/user/controller/UserController.java
+++ src/main/java/springcloudstudy/usermicroservices/api/user/controller/UserController.java
@@ -2,11 +2,16 @@ package springcloudstudy.usermicroservices.api.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import springcloudstudy.usermicroservices.api.user.common.Simple;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springcloudstudy.usermicroservices.api.common.Simple;
+import springcloudstudy.usermicroservices.api.exception.dto.ErrorDto;
+import springcloudstudy.usermicroservices.api.user.controller.dto.ResponseUserDto;
+import springcloudstudy.usermicroservices.api.user.controller.dto.UserDto;
+import springcloudstudy.usermicroservices.domain.user.entity.User;
+import springcloudstudy.usermicroservices.domain.user.service.LoginService;
 
 @Slf4j
 @RestController
@@ -16,6 +21,7 @@ public class UserController {
 
 //    private final Environment env;
     private final Simple simple;
+    private final LoginService loginService;
 
     @GetMapping("/health-check")
     public String status(){
@@ -26,5 +32,22 @@ public class UserController {
     public String hello(){
 //        String message = env.getProperty("simple.message");
         return simple.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@Validated @RequestBody UserDto userDto){
+        Object result = loginService.createUser(userDto);
+        HttpStatus httpStatus = HttpStatus.CREATED;
+
+        if(result instanceof ErrorDto){
+            httpStatus = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(result, httpStatus);
+        }
+
+        User createdUser = (User) result;
+        ResponseUserDto responseUserDto =
+                new ResponseUserDto(createdUser.getEmail(), createdUser.getUsername(), createdUser.getUserId());
+//        return new ResponseEntity<>(responseUserDto, httpStatus);
+        return ResponseEntity.status(201).body(responseUserDto);
     }
 }
