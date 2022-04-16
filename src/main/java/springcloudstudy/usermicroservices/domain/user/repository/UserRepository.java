044d9758ca620142f8d53bda72385cc449a07e05
최영찬 <gks54410@gmail.com>
2022-04-16 22:43:55 +0900

    Feat: 사용자 고유 Id 조회 추가

--- src/main/java/springcloudstudy/usermicroservices/domain/user/repository/UserRepository.java
+++ src/main/java/springcloudstudy/usermicroservices/domain/user/repository/UserRepository.java
@@ -3,8 +3,12 @@ package springcloudstudy.usermicroservices.domain.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import springcloudstudy.usermicroservices.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String getName);
 
     User findByEmail(String getEmail);
+
+    Optional<User> findByUserId(String userId);
 }
