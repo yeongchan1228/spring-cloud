c204e624fb04ecf278fc874e0a4810ac166011f6
최영찬 <gks54410@gmail.com>
2022-04-16 19:51:04 +0900

    Feat: 회원 가입 처리 기능 추가

---
+++ src/main/java/springcloudstudy/usermicroservices/domain/user/service/LoginServiceImpl.java
@@ -0,0 +1,52 @@
+package springcloudstudy.usermicroservices.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springcloudstudy.usermicroservices.api.exception.dto.ErrorDetailDto;
+import springcloudstudy.usermicroservices.api.exception.dto.ErrorDto;
+import springcloudstudy.usermicroservices.api.user.controller.dto.UserDto;
+import springcloudstudy.usermicroservices.domain.user.entity.User;
+import springcloudstudy.usermicroservices.domain.user.repository.UserRepository;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LoginServiceImpl implements LoginService{
+
+    private final UserRepository userRepository;
+
+    @Transactional // SpringDataJpa에 Transactional이 붙어 있어서 없어도 된다.
+    public Object createUser(UserDto userDto){
+        String uuid = UUID.randomUUID().toString();
+        String getName = userDto.getName();
+        String getEmail = userDto.getEmail();
+
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setMessage("Unique Error!");
+        checkDuplicate(getName, getEmail, errorDto);
+        if(errorDto.getErrors().size() != 0){
+            return errorDto;
+        }
+
+        User user = User.createUser()
+                .userId(uuid)
+                .email(getEmail)
+                .encryptedPwd("encryptPwd" + uuid)
+                .username(getName)
+                .build();
+        userRepository.save(user);
+        return user;
+    }
+
+    private void checkDuplicate(String getName, String getEmail, ErrorDto errorDto) {
+        if(userRepository.findByUsername(getName) != null){
+            errorDto.getErrors().add(new ErrorDetailDto("Name", "Name already used"));
+        }
+        if(userRepository.findByEmail(getEmail) != null){
+            errorDto.getErrors().add(new ErrorDetailDto("Email", "Email already used"));
+        }
+    }
+}
