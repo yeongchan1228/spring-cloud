40e5a43c4c6bca363744310438c92d3dab790aab
최영찬 <gks54410@gmail.com>
2022-04-16 22:48:44 +0900

    Initial Commit

---
+++ src/main/java/springcloudstudy/catalogsservice/CatalogsServiceApplication.java
@@ -0,0 +1,14 @@
+package springcloudstudy.catalogsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class CatalogsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springcloudstudy/catalogsservice/CatalogsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloudstudy.catalogsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
