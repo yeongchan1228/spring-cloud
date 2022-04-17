cae9a8f89b414b7af60f684f219df13c45fac8a4
최영찬 <gks54410@gmail.com>
2022-04-17 20:41:49 +0900

    Feat: Order Entity 추가

---
+++ src/main/java/springcloudstudy/ordersservice/domain/order/entity/Order.java
@@ -0,0 +1,56 @@
+package springcloudstudy.ordersservice.domain.order.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * implements Serializable
+ * 다른 네트워크로 전송하기 위해서, 마샬링 작업
+ * 객체 데이터를 바이트스트림으로 변환
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(value = AuditingEntityListener.class)
+@Table(name = "orders")
+public class Order implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, updatable = true)
+    private String productId;
+    @Column(nullable = false)
+    private Integer qty;
+    @Column(nullable = false)
+    private Integer unitPrice;
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @CreatedDate
+    @Column(updatable = false, insertable = false)
+    private LocalDate createdDate;
+
+    @Builder(builderMethodName = "createOrder")
+    public Order(String productId, Integer qty, Integer unitPrice, Integer totalPrice, String userId, String orderId) {
+        this.productId = productId;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+        this.totalPrice = totalPrice;
+        this.userId = userId;
+        this.orderId = orderId;
+    }
+}
