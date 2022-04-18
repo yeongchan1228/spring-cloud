ae41564f13ca774058db31ba02bf17d167334b84
최영찬 <gks54410@gmail.com>
2022-04-18 12:03:14 +0900

    Feat: Jwt 생성 및 반환 기능 추가

--- src/main/java/springcloudstudy/usermicroservices/security/filter/AuthenticationFilter.java
+++ src/main/java/springcloudstudy/usermicroservices/security/filter/AuthenticationFilter.java
@@ -1,25 +1,53 @@
 package springcloudstudy.usermicroservices.security.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springcloudstudy.usermicroservices.api.exception.user.NotFoundUserException;
 import springcloudstudy.usermicroservices.api.user.controller.dto.RequestLoginDto;
+import springcloudstudy.usermicroservices.domain.user.service.UserService;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 
 /**
  * UsernamePasswordAuthenticationFilter는
  * /login 요청시 동작하는 필터
  */
+@Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Key key;
+    private Environment env;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService,
+                                Environment env) {
+        super(authenticationManager);
+        this.userService = userService;
+        this.env = env;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response) throws AuthenticationException {
@@ -50,6 +78,25 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
-//        super.successfulAuthentication(request, response, chain, authResult);
+
+        User user = (User) authResult.getPrincipal();
+        String userEmail = user.getUsername();
+
+        springcloudstudy.usermicroservices.domain.user.entity.User findUser =
+                userService.getUserByEmail(userEmail).orElseThrow(() -> new NotFoundUserException("유저를 찾을 수 없습니다."));
+        String userId = findUser.getUserId();
+
+        // secret 키는 Base64인코딩이 안되어 있기 때문에 StandardCharsets.UTF_8로 형태를 알려주어야 한다.
+        // Base64로 인코딩 되어 있을 시 Decoders.BASE64.decode(secret)
+        byte[] keyBytes = env.getProperty("token.secret").getBytes(StandardCharsets.UTF_8);
+        key = Keys.hmacShaKeyFor(keyBytes);
+        String token = Jwts.builder()
+                .setSubject(userId)
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(env.getProperty("token.expirationTime"))))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userId);
     }
 }
