323ca4f6aec42b9b66ee05e0c268dd44ee1ed002
최영찬 <gks54410@gmail.com>
2022-04-18 12:04:35 +0900

    Feat: 사용자 이메일을 통한 조회 추가

--- src/main/java/springcloudstudy/usermicroservices/domain/user/service/UserService.java
+++ src/main/java/springcloudstudy/usermicroservices/domain/user/service/UserService.java
@@ -2,7 +2,10 @@ package springcloudstudy.usermicroservices.domain.user.service;
 
 import springcloudstudy.usermicroservices.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserService {
     User getUserByUserId(String userId);
     Iterable<User> getAllUser();
+    Optional<User> getUserByEmail(String email);
 }
