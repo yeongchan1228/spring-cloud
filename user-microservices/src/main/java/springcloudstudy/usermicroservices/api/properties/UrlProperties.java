7d9a6508516936f3e7cc67a9a84af35c302c754b
최영찬 <gks54410@gmail.com>
2022-04-27 15:01:25 +0900

    Feat: url 설정 정보 저장

--- user-microservices/src/main/java/springcloudstudy/usermicroservices/api/properties/UrlProperties.java
+++ user-microservices/src/main/java/springcloudstudy/usermicroservices/api/properties/UrlProperties.java
@@ -1,2 +1,21 @@
-package springcloudstudy.usermicroservices.api.properties;public class UrlProperties {
+package springcloudstudy.usermicroservices.api.properties;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConfigurationProperties(prefix = "url")
+@ConstructorBinding
+public class UrlProperties {
+    @JsonProperty("order-service")
+    private final String orderService;
+    @JsonProperty("order-service-direct")
+    private final String orderServiceDirect;
+
+    public UrlProperties(String orderService, String orderServiceDirect) {
+        this.orderService = orderService;
+        this.orderServiceDirect = orderServiceDirect;
+    }
 }
