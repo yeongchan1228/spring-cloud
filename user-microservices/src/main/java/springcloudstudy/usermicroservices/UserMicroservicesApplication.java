4461635021faaedeadbfd2355a3df8f68a15c346
최영찬 <gks54410@gmail.com>
2022-05-04 11:38:27 +0900

    Feat: FeignClient 활성화

--- user-microservices/src/main/java/springcloudstudy/usermicroservices/UserMicroservicesApplication.java
+++ user-microservices/src/main/java/springcloudstudy/usermicroservices/UserMicroservicesApplication.java
@@ -1,19 +1,23 @@
 package springcloudstudy.usermicroservices;
 
+import feign.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import springcloudstudy.usermicroservices.api.feign.decoder.FeignErrorDecoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing
+@EnableFeignClients(basePackages = "springcloudstudy.usermicroservices.api.feign")
 @ConfigurationPropertiesScan(basePackages = "springcloudstudy.usermicroservices.api.properties")
 public class UserMicroservicesApplication {
 
@@ -35,5 +39,17 @@ public class UserMicroservicesApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	// Feign Client 로깅 레벨 등록
+	@Bean
+	public Logger.Level feignLoggerLevel(){
+		return Logger.Level.FULL;
+	}
+
+	// Error Decoder 추가
+	@Bean
+	public FeignErrorDecoder addFeignErrorDecoder(){
+		return new FeignErrorDecoder();
 	}
 }
